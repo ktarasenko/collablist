7c64e2b8fc6b2b20e6b80d6b6a83b32550ce95ef
Konstantin Tarasenko <konstantin.v.tarasenko@gmail.com>
2012-11-07 18:14:40 +0200

    db support
    h2 db dependency added
    tuned pom file to take control over lift version

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -6,6 +6,8 @@ import _root_.net.liftweb.http._
 import _root_.net.liftweb.sitemap._
 import _root_.net.liftweb.sitemap.Loc._
 import Helpers._
+import net.liftweb.db._
+import java.sql._
 
 /**
   * A class that's instantiated early and run.  It allows the application
@@ -15,10 +17,32 @@ class Boot {
   def boot {
     // where to search snippet
     LiftRules.addToPackages("com.collablist.web")
+    
+    if (!DB.jndiJdbcConnAvailable_?) DB.defineConnectionManager(DefaultConnectionIdentifier, DBVendor)
 
     // Build SiteMap
     val entries = Menu(Loc("Home", List("index"), "Home")) :: Nil
     LiftRules.setSiteMap(SiteMap(entries:_*))
   }
+  
+  object DBVendor extends ConnectionManager {
+  def newConnection(name: ConnectionIdentifier): Box[Connection] = {
+    try {
+      /** Uncomment if you really want Derby
+       * 
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver")
+      val dm = DriverManager.getConnection("jdbc:derby:pca_example;create=true")
+      */
+
+      Class.forName("org.h2.Driver")
+      val dm = DriverManager.getConnection("jdbc:h2:pca_example")
+      Full(dm)
+    } catch {
+      case e : Exception => e.printStackTrace; Empty
+    }
+  }
+  def releaseConnection(conn: Connection) {conn.close}
+}
+
 }
 
